a26f7ecfa210dbc5f2a5d36c3168b79520f97206
EbotProg <achaleebotoma2002@gmail.com>
2025-06-03 15:42:33 +0100

    updated video modal

--- src/components/organisms/VideoModal.organism.tsx
+++ src/components/organisms/VideoModal.organism.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import useClickOutside from "@/lib/hooks/useClickOutside";
 
 interface VideoModalProps {
   videoUrl: string; // URL of the video to be played
@@ -7,6 +8,9 @@ interface VideoModalProps {
 }
 
 const VideoModal = ({ videoUrl, handleClose }: VideoModalProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useClickOutside(ref, handleClose);
+
   return (
     <div className="backdrop-blur-[10px] bg-black/20 opacity-100 w-full h-screen z-50 fixed top-0 left-0">
       <div className="flex relative w-full h-full">
@@ -22,19 +26,21 @@ const VideoModal = ({ videoUrl, handleClose }: VideoModalProps) => {
           }}
         />
         <div className="place-self-center w-full">
-          <video
-            controls
-            style={{
-              width: "100%",
-              maxWidth: "600px",
-              border: "1px solid #ccc",
-              borderRadius: "8px",
-              placeSelf: "center",
-            }}
-          >
-            <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <div ref={ref} className="w-fit place-self-center">
+            <video
+              controls
+              style={{
+                width: "100%",
+                maxWidth: "600px",
+                border: "1px solid #ccc",
+                borderRadius: "8px",
+                placeSelf: "center",
+              }}
+            >
+              <source src={videoUrl} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
       </div>
     </div>
